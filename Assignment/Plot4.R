467934a57ec2bd8584d374b961b1c4f103c7bdff
rbonifac <rbonifac1@yahoo.com>
2016-05-16 10:39:49 -0400

    Initial release

--- Assignment/Plot4.R
+++ Assignment/Plot4.R
@@ -8,7 +8,7 @@ points(epData$Datetime,epData$Sub_metering_2,type="l",col="red")
 points(epData$Datetime,epData$Sub_metering_3,type="l",col="blue")
 legend("topright", col = c("black","red","blue"), 
        legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),
-       bty="n",lwd=1,y.intersp=.75)
+       bty="n",lwd=1,y.intersp=.65,xjust=1,yjust=1)
        
 plot(Datetime,Global_reactive_power,type="l")
 })
